638680b02071f5167c432d23f1697893540e4420
pinkrain3 <105021086@live.asia.edu.tw>
2016-10-12 12:03:41 +0800

    number cla

--- src/ex/ex01_1040210XX.java
+++ src/ex/ex01_1040210XX.java
@@ -5,11 +5,24 @@
  * Author: 1040210XX 周永振老師
  */
 
+import java.util.Scanner;
+
 public class ex01_1040210XX {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in) ;
+		int a, b ;
+		System.out.print("請輸入一串數字 : ");
+		a = scn.nextInt();
+		b = scn.nextInt();
+		int i = a , sum = 0 ;
+		while (i <= b){
+			sum += i;
+			i++;
+		}
+		System.out.print("字串加總為:" +sum);
 	}
-
+	
 }
+
